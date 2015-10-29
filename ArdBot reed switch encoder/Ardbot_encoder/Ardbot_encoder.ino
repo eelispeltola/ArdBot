5376402d487493c6c05efe76a21a22ef9a10d11c
Eelis Peltola <eelispeltola@users.noreply.github.com>
2015-10-29 15:37:28 +0200

    Initial commit

---
+++ ArdBot reed switch encoder/Ardbot_encoder/Ardbot_encoder.ino
@@ -0,0 +1,24 @@
+/* ArdBot RPM encoder
+ * Arduino Nano, ATMega328
+ * Takes a signal that clicks on repeatedly and converts it to 
+ * RPM measurements.
+ * Started 12.09.2015
+ * Last Modified 20.09.2015
+ * epe
+ */
+
+class Rpm {
+  public:
+  private:
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
