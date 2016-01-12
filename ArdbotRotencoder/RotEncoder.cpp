26efa47cd23506c33101b56c6a7297e428dab74f
Eelis Peltola <eelispeltola@users.noreply.github.com>
2016-01-12 22:18:03 +0200

    Changed word to unsigned int, cleaned up redundant code

--- ArdbotRotencoder/RotEncoder.cpp
+++ ArdbotRotencoder/RotEncoder.cpp
@@ -6,7 +6,7 @@ Takes a signal that clicks on and off repeatedly and converts it to
 various RPM-type measurements.
 
 Created 12.09.2015
-Modified 11.01.2016
+Modified 12.01.2016
 For Arduino Nano, ATmega328
 By epe
 */
@@ -14,52 +14,30 @@ By epe
 #include "RotEncoder.h"
 #include "Arduino.h"
 
-namespace {
-
-}
-
-// ISR adds 1 to rotation count.
-// TODO: Move to main program (ArdBot)
-/*void RotEncoder::trigger() {
-if ((millis() - _lastDebounce) > _debounce) {
-_tickcount++;
-_lastDebounce = millis();
-}
-}*/
 
 RotEncoder::RotEncoder(const float wheel_d, const int num_of_encs) {
-	//	_pin = pin;  // Input pin, 2 or 3 (ATmega328)
 	_circ = wheel_d * 3.1415926;  // Circumference of wheel in mm (2*pi*r = pi*d)
 	_encs = num_of_encs;  // Number of encoding triggers per wheel
-						  //	_tickcount = 0;
-						  //	_debounce = debounce_delay;  // Delay for debounce control in ms
-						  //	_lastDebounce = 0;
 }
 
 
 // Returns number of rotations, rounded to the nearest integer.
-// TODO: Change to word for speed if too slow.
-volatile float RotEncoder::count(volatile word clicks) {
+// TODO: Change to unsigned int for speed if too slow.
+volatile float RotEncoder::count(volatile unsigned int clicks) {
 	volatile float rotations{ clicks / _encs };
 	return rotations;
 }
 
 // Returns distance in meters.
-float RotEncoder::distance(volatile word clicks) {
+float RotEncoder::distance(volatile unsigned int clicks) {
 	float dist{ _circ * count(clicks) * 0.001 };
 	return dist;
 }
 
-// Clears tick count.
-// TODO: Change to clear something else?? Or scrap
-/*void RotEncoder::clear() {
-_tickcount = 0;
-}*/
-
 // Returns velocity in (m/s).
 // TODO: Test if any difference with millis() timekeeping.
 // TODO: v2 with no delay(), time automatically from loop()?
-float RotEncoder::velocity(volatile word clicks) {
+float RotEncoder::velocity(volatile unsigned int clicks) {
 	float oldDist{ distance(clicks) };
 	delay(300);
 	float newDist{ distance(clicks) };
@@ -67,3 +45,8 @@ float RotEncoder::velocity(volatile word clicks) {
 	return velo;
 }
 
+// Clears tick count.
+// TODO: Change to clear something else?? Or scrap
+// void RotEncoder::clear() {
+//   _tickcount = 0;
+// }
--- ArdbotRotencoder/RotEncoder.h
+++ ArdbotRotencoder/RotEncoder.h
@@ -6,7 +6,7 @@ Takes a signal that clicks on and off repeatedly and converts it to
 various RPM-type measurements.
 
 Created 12.09.2015
-Modified 11.01.2016
+Modified 12.01.2016
 For Arduino Nano, ATmega328
 By epe
 */
@@ -14,24 +14,19 @@ By epe
 #ifndef ROTENCODER_H
 #define ROTENCODER_H
 
+
 #include "Arduino.h"
 
-// TODO: Clear up when tested in code
 class RotEncoder {
 public:
 	RotEncoder(const float wheel_d, const int num_of_encs);
-	volatile float count(volatile word clicks);
-	float distance(volatile word clicks);
-	//		void clear();
-	float velocity(volatile word clicks);
-	//		void trigger();
+	volatile float count(volatile unsigned int clicks);
+	float distance(volatile unsigned int clicks);
+	float velocity(volatile unsigned int clicks);
 private:
-	//		int _pin;
 	float _circ;
 	int _encs;
-	//		volatile word _tickcount;
-	//		unsigned long _debounce;
-	//		unsigned long _lastDebounce;
 };
+
 
 #endif // ROTENCODER_H
