24c15052aeea78fa4c65c5eeb2fe733d9e34f422
Eelis Peltola <eelispeltola@users.noreply.github.com>
2016-02-20 02:07:12 +0200

    ArdBot RotEncoder: Fixed rotations to output non-whole numbers and velocity to output (m/s)

--- ArdbotRotencoder/RotEncoder.cpp
+++ ArdbotRotencoder/RotEncoder.cpp
@@ -6,7 +6,7 @@ Takes a signal that clicks on and off repeatedly and converts it to
 various RPM-type measurements.
 
 Created 12.09.2015
-Modified 16.01.2016
+Modified 20.02.2016
 For Arduino Nano, ATmega328
 By epe
 */
@@ -24,7 +24,7 @@ RotEncoder::RotEncoder(const float wheel_d, const int num_of_encs) {
 // Returns number of rotations.
 // TODO: Change to unsigned int for speed if too slow.
 volatile float RotEncoder::rotations(volatile unsigned int clicks) {
-	volatile float rotations{ clicks / _encs };
+	volatile float rotations{ (float)clicks / (float)_encs };
 	return rotations;
 }
 
@@ -34,11 +34,12 @@ float RotEncoder::distance(volatile unsigned int clicks) {
 	return dist;
 }
 
-// Returns velocity in (m/s). Requires main loop to run faster than <<interval>>
+// Returns velocity in (m/s). Requires main loop to run faster than <<interval>>.
+// Pass <<oldDistance>> as meters and interval as milliseconds.
 float RotEncoder::velocity(volatile unsigned int clicks, unsigned long interval,
 						   float& oldDistance) {
 	float newDist{ distance(clicks) };
-	float velo{ (newDist - oldDistance) / interval };
+	float velo{ (newDist - oldDistance)*1000 / interval };
 	oldDistance = newDist;
 	return velo;
 }
