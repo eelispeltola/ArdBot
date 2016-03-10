2e6a558497015f1fc3a4ab0807f32a0d1fa7ed6d
Eelis Peltola <eelis.peltola@gmail.com>
2016-03-11 01:11:34 +0200

    Teenbot: Add remap_images and set_video_sizes -functions to rpidispmap.py

--- Teenbot01/rpidispmap.py
+++ Teenbot01/rpidispmap.py
@@ -36,3 +36,53 @@ def calculate_maps(image_size):
     return mapl_x, mapl_y, mapr_x, mapr_y
 
 
+# Read videos and remap read frames based on undistort maps
+def remap_images(l_video, r_video, leftmap_x, leftmap_y,
+                 rigthmap_x, rightmap_y):
+    vidreadl, leftframe = l_video.read()
+    vidreadr, rightframe = r_video.read()
+    if not vidreadl:
+        print("Couldn't grab frame from left camera")
+        return
+
+    if not vidreadr:
+        print("Couldn't grab frame from right camera")
+        return
+
+    remappedimgl = remap(src=leftframe, map1=leftmap_x, map2=leftmap_y,
+                         interpolation=INTER_LINEAR)
+    imshow("Remapped left", remappedimgl)
+
+    remappedimgr = remap(src=rightframe, map1=rigthmap_x, map2=rightmap_y,
+                         interpolation=INTER_LINEAR)
+    imshow("Remapped right", remappedimgr)
+
+    return remappedimgl, remappedimgr, leftframe, rightframe
+
+
+# Set video feeds to <<video_size>> or closest equivalent
+# (set video frames to 144p for performance)
+def set_video_sizes(left_vid, right_vid, video_size):
+    print("Unmodified left vid size: ")
+    print(left_vid.get(CAP_PROP_FRAME_WIDTH))
+    print(" x ")
+    print(left_vid.get(CAP_PROP_FRAME_HEIGHT))
+
+    print("Unmodified right vid size: ")
+    print(right_vid.get(CAP_PROP_FRAME_WIDTH))
+    print(" x ")
+    print(right_vid.get(CAP_PROP_FRAME_HEIGHT))
+
+    width = video_size[0]
+    height = video_size[1]
+    left_vid.set(CAP_PROP_FRAME_WIDTH, width)
+    left_vid.set(CAP_PROP_FRAME_HEIGHT, height)
+    right_vid.set(CAP_PROP_FRAME_WIDTH, width)
+    right_vid.set(CAP_PROP_FRAME_HEIGHT, height)
+
+    print("Modified video size: ")
+    print(right_vid.get(CAP_PROP_FRAME_WIDTH))
+    print(" x ")
+    print(right_vid.get(CAP_PROP_FRAME_HEIGHT))
+
+
